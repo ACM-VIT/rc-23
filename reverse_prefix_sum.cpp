a79e87d1cf04d0dc38ef83ca0834d14b98f6da76
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-03-22 14:39:34 +0530

    update: add exception handling for reverse prefix sum
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- reverse_prefix_sum.cpp
+++ reverse_prefix_sum.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 
-int main(){
-	int t;
-	std::cin>>t;
-	while(t--){
-		int n;
-		std::cin>>n;
-		int s[10];
-		for(int i=0;i<n;i++){
-			std::cin>>s[i];
-		}
-		int a[10];
-		for(int i=0;i<n;i++){
-			int temp = s[i];
-			for(int j=0;j<i;j++){
-				temp = temp - a[j];
-			}
-			a[i] = temp;
-		}
-		for(int i=0;i<n;i++){
-			std::cout<<a[i]<<" ";
-		}
-		std::cout<<"\n";
+int solve(int n){
+	int s[n];
+	for(int i=0;i<n;i++){
+		std::cin>>s[i];
 	}
+	int a[n];
+	for(int i=0;i<n;i++){
+		int temp = s[i];
+		for(int j=0;j<i;j++){
+			temp = temp - a[j];
+		}
+		a[i] = temp;
+	}
+	for(int i=0;i<n;i++){
+		std::cout<<a[i]<<" ";
+	}
+	std::cout<<"\n";
+}
+
+int main()
+{
+	try
+	{
+    	int test;
+    	std::cin >> test;
+    	if (!std::cin || test < 1 || test > 150)
+        	throw -1;
+    	for (int t = 0; t < test; t++)
+    	{
+        	long long n;
+        	std::cin >> n ;
+        	if (!std::cin || n < 1 || n > 10000)
+            	throw -2;
+        	solve(n);
+    	}
+	}
+	catch (...)
+	{
+    	std::cout << "Invalid input. Please check the question description.\n";
+	}
+	return 0;
 }
