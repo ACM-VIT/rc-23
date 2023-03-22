4218c1251223b61e422e038c16de015f526da1d3
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-03-22 14:19:59 +0530

    error: golden still broken
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- golden.cpp
+++ golden.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 
-#define phi 1.618033988749894
-
 void solveMyProblem(istream &cin, ostream &cout) {
+    double phi = 1.618033988749894;
     try{
         // declare variables here
         int n1, n2;
@@ -17,12 +17,12 @@ void solveMyProblem(istream &cin, ostream &cout) {
         {
             throw -1;
         }
-        vector<int> num1(n1);
+        int num1[n1];
         for (int i = 0; i < n1; i++)
         {   
             int inp;
             cin >> inp;            
-            if (inp < 10000 || inp > 100000 || !std::cin){
+            if (inp < 1 || inp > 100000 || !std::cin){
                 throw -1;
             }
             num1[i];
@@ -32,34 +32,35 @@ void solveMyProblem(istream &cin, ostream &cout) {
         {
             throw -1;
         }
-        vector<int> num2(n2);
+        int num2[n2];
         for (int i = 0; i < n2; i++)
         {
             int inp;
             cin >> inp;
-            if (inp < 10000 || inp > 100000 || !std::cin){
+            if (inp < 1 || inp > 100000 || !std::cin){
                 throw -1;
             }
             num2[i];
         }
         // solve here
-        unsigned long long difference = 1000000000000000;
-        double ratio = 0, distance = 0, a = 0, b = 0;
-        int i = 0, j = 0;
+        float difference;
+        int a,b;
         for (int i = 0; i < n1; i++)
-        {
+        {   
+            float ratio1 = 0, ratio2 = 0;
+            int distance = 0;
             for (int j = 0; j < n2; j++)
             {
-                double ratio;
-                ratio = (double) num1[i] / (double) num2[j];
-                double distance = 0;
-                distance = abs(ratio - phi);
-                if (distance < difference)
+                ratio1 = num1[i] / (float) num2[j];
+                ratio2 = num2[j] / (float) num1[i];
+                distance = min(abs(ratio1 - phi), abs(ratio2 - phi));
+                if (distance <= difference)
                 {
                     difference = distance;
-                    double a = (int)num1[i];
-                    double b = (int)num2[j];
+                    a = num1[i];
+                    b = num2[j];
                 }
+                // cout << a << "," << b << endl;
             }  
         }
         cout << a << "/" << b << ":" << difference << endl; 
