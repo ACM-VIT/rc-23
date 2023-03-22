852004040fdaac8e36d449c56acc62c035feb03d
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-03-22 14:35:07 +0530

    update: E1 and E2 do not have testcase handling update: n-times-after... error handling updated
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- n-times-after-increment-problem.cpp
+++ n-times-after-increment-problem.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 
-using namespace std;
-
-int main() {
-  double a, m, n, x;
-  cin >> a >> m >> n;
-
-  while (n == 1) {
-    cout << "The third input cannot be equal to 1. Please enter a different value: ";
-    cin >> n;
-  }
-
+int solve(int a, int m, double n) {
+  double x;
   x = ((m*n)-a-m)/(1-n);
-  cout << x;	
-  
-  /*
-   x, x+a
-   x+a+m = n*(x+m)
-   x = ((m*n)-a-m)/(1-n)
-  */
+  std::cout << x;	
+}
 
+int main()
+{
+  try
+  {
+      int test;
+      std::cin >> test;
+      if (!std::cin || test < 1 || test > 150)
+          throw -1;
+      for (int t = 0; t < test; t++)
+      {
+          long long a, m;
+          double n;
+          std::cin >> a >> m >> n;
+          if (a < 1 || a > 100000 || m < 1 || m > 100000 || n <= 0 || n == 1 || n > 100000)
+              throw -2;
+          solve(a,m,n);
+      }
+  }
+  catch (...)
+  {
+      std::cout << "Invalid input. Please check the question description.\n";
+  }
   return 0;
 }
