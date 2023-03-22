68941bcecce8c7f94e3e18884c23999755edb248
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-03-22 13:44:10 +0530

    fix: input validation for golden
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- golden.cpp
+++ golden.cpp
@@ -8,47 +8,66 @@ using namespace std;
 #define phi 1.618033988749894
 
 void solveMyProblem(istream &cin, ostream &cout) {
-    // declare variables here
-    int n1, n2;
-    vector<int> num1(n1);
-    // take inputs here
-    cin >> n1;
-    for (int i = 0; i < n1; i++)
-    {   
-        int inp;
-        cin >> inp;
-        num1[i];
-    }
-    cin >> n2;
-    vector<int> num2(n2);
-    for (int i = 0; i < n2; i++)
-    {
-        int inp;
-        cin >> inp;
-        num2[i];
-    }
-
-    // solve here
-    unsigned long long difference = 1000000000000000;
-    double ratio = 0, distance = 0, a = 0, b = 0;
-    int i = 0, j = 0;
-    for (int i = 0; i < n1; i++)
-    {
-        for (int j = 0; j < n2; j++)
+    try{
+        // declare variables here
+        int n1, n2;
+        // take inputs here
+        cin >> n1;
+        if (n1 < 3 || n1 > 15 || !std::cin)
         {
-            double ratio;
-            ratio = (double) num1[i] / (double) num2[j];
-            double distance = 0;
-            distance = abs(ratio - phi);
-            if (distance < difference)
-            {
-                difference = distance;
-                double a = (int)num1[i];
-                double b = (int)num2[j];
+            throw -1;
+        }
+        vector<int> num1(n1);
+        for (int i = 0; i < n1; i++)
+        {   
+            int inp;
+            cin >> inp;            
+            if (inp < 10000 || inp > 100000 || !std::cin){
+                throw -1;
             }
-        }  
+            num1[i];
+        }
+        cin >> n2;
+        if (n2 < 3 || n2 > 15 || !std::cin)
+        {
+            throw -1;
+        }
+        vector<int> num2(n2);
+        for (int i = 0; i < n2; i++)
+        {
+            int inp;
+            cin >> inp;
+            if (inp < 10000 || inp > 100000 || !std::cin){
+                throw -1;
+            }
+            num2[i];
+        }
+        // solve here
+        unsigned long long difference = 1000000000000000;
+        double ratio = 0, distance = 0, a = 0, b = 0;
+        int i = 0, j = 0;
+        for (int i = 0; i < n1; i++)
+        {
+            for (int j = 0; j < n2; j++)
+            {
+                double ratio;
+                ratio = (double) num1[i] / (double) num2[j];
+                double distance = 0;
+                distance = abs(ratio - phi);
+                if (distance < difference)
+                {
+                    difference = distance;
+                    double a = (int)num1[i];
+                    double b = (int)num2[j];
+                }
+            }  
+        }
+        cout << a << "/" << b << ":" << difference << endl; 
     }
-    cout << a << "/" << b << ":" << difference << endl; 
+    catch (int e)
+    {
+        cout << "Input out of range." << endl;
+    }
 }
 
 int main() {
